014696698ccecf369d705a98ac488e2c8b3db4c3
sean-camara <camara.sean13@gmail.com>
2025-04-24 21:58:29 +0800

    Add files via upload

---
+++ login_register/script.js
@@ -0,0 +1,5 @@
+//when a tag is clicked it will change appropriately
+function showForm(formId) {
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
+}
